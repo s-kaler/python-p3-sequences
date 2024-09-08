7b22f026ba1294473fdf10e4494d3170294fc54d
Sundeep <sundeep.kaler@gmail.com>
2024-09-07 22:48:25 -0400

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fib_list = []
+    for x in range(length):
+        if(x == 0):
+            fib_list.append(0)
+        elif(x == 1):
+            fib_list.append(1)
+        else:
+            fib_list.append(fib_list[x-1] + fib_list[x-2])
+
+    print(fib_list)
+    
     pass
